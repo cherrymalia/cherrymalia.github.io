9e260a99d5f5d088e5407d62dedd0682ec1bf00f
cherrymalia <35239061+cherrymalia@users.noreply.github.com>
2022-10-04 21:13:02 -0400

    validation done, fixing colors for messages

--- validation.js
+++ validation.js
@@ -1,71 +1,6 @@
-
-let count = 0;
-/*
-function validateUsername() {
-  let user = document.getElementById("name").value;
-  let out = document.getElementById("output");
-  
-}
-
-function validateEmail() {
-  let user = document.getElementById("email").value;
-  let out = document.getElementById("output");
-  if (user == "") {
-    out.innerText += "email EMPTY\n";
-  } else if(user.length < 4 || user.length > 12) {
-    out.innerText += "email WRONG\n";
-  } else {
-    count++;
-  }
-}
-
-function validateNumber() {
-  let user = document.getElementById("phone").value;
-  let out = document.getElementById("output");
-  if (user == "") {
-    out.innerText += "number EMPTY\n";
-  } else if(user.length < 4 || user.length > 12) {
-    out.innerText += "number WRONG\n";
-  } else {
-    count++;
-  }
-}
-
-function validatePass() {
-  let user = document.getElementById("pass").value;
-  let out = document.getElementById("output");
-  if (user == "") {
-    out.innerText += "pass EMPTY\n";
-  } else if(user.length < 4 || user.length > 12) {
-    out.innerText += "pass WRONG\n";
-  } else {
-    count++;
-  }
-}
-
-function validateConfirmPass()  {
-  let user = document.getElementById("conf_pass").value;
-  let out = document.getElementById("output");
-  if (user == "") {
-    out.innerText = "c pass EMPTY\n";
-  } else if(user.length < 4 || user.length > 12) {
-    out.innerText = "c pass WRONG\n";
-  } else {
-    count++;
-  }
-}
-*/
-
 function validateForm() {
-  /*
-  validateUsername();
-  validateEmail();
-  validateNumber();
-  validatePass();
-  validateConfirmPass();
-  */
-
   //declaring variables
+  var count = 0;
   let user = document.infoForm.name.value;
   let email = document.infoForm.email.value;
   let phone = document.infoForm.phone.value;
@@ -76,47 +11,81 @@ function validateForm() {
   let out = document.getElementById("output");
   out.innerText = "";
 
+  //for username validations
   if (user == "") {
-    out.innerText += "name EMPTY\n";
+    out.innerText += "Please enter username\n";
   } else if(user.length < 4 || user.length > 12) {
-    out.innerText += "name WRONG\n";
+    out.innerText += "Please enter a valid username\n";
+  } else {
+    count++;
   }
 
   //for email validation
   if (email == "") {
-    out.innerText += "email EMPTY\n";
+    out.innerText += "Please enter email\n";
   } else {
     if (/^\S+@\S+\.\S+$/.test(email)) {
       let ending = email.substring(email.length - 3);
-      if (ending == "edu" || ending == "com" || ending == "org" || ending == "net") {
+      if (ending == "edu" || ending == "com" || ending == "org" || ending == "net") { 
         count++;
       } else {
-        out.innerText += "email WRONG\n";
+        out.innerText += "Please enter a valid email\n";
       }
     } else {
-      out.innerText += "email WRONG\n";
+      out.innerText += "Please enter a valid email\n";
     }
   }
 
   //for phone number validation
   if (phone == "") {
-    out.innerText += "number EMPTY\n";
+    out.innerText += "Please enter phone number\n";
   } else {
     if(/^\(\d{3}\)-\d{3}-\d{4}$/.test(phone)) {
       count++;
     } else {
-      out.innerText += "number WRONG\n";
+      out.innerText += "Please enter a valid phone number\n";
     }
   } 
 
+  //for password validation
+  if (pass == "") {
+    out.innerText += "Please enter password\n";
+  } else {
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(pass)) {
+      count++;
+    } else {
+      out.innerText += "Please enter a valid password\n";
+    }
+  }
+
+  //for confirm password validation
+  if (conf_pass == "") {
+    out.innerText += "Please confirm password\n";
+  } else {
+    if (conf_pass == pass && /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(conf_pass)) {
+      count++;
+    } else {
+      out.innerText += "Please confirm password\n";
+      alert("Passwords do not match");
+    }
+  }
+  
   //for gender validation
   if (gender == "") {
-    out.innerText += "gender EMPTY\n";
+    out.innerText += "Please select gender\n";
+  } else {
+    count++;
   }
 
   //for age validation
   if (age == "") {
-    out.innerText += "age EMPTY\n";
+    out.innerText += "Please select age group\n";
+  } else {
+    count++;
+  }
+
+  if (count == 7) {
+    window.location.href = 'index.html'
   }
 }
 
